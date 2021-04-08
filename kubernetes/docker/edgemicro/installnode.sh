c3dac4aa7cc4d75a48773d7f8ac6497a5b5d9792
Sumit Parakh <sparakh@google.com>
2021-04-08 17:52:57 +0000

    chore: testing docker setup

--- kubernetes/docker/edgemicro/installnode.sh
+++ kubernetes/docker/edgemicro/installnode.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set echo off
 
-npm install -g git://github.com/sumitparakh/microgateway.git#docker-dev-setup --registry=https://registry.npmjs.org
+
+cd /opt/apigee
+git clone https://github.com/sumitparakh/microgateway.git
+cd microgateway
+git checkout docker-dev-setup
+npm install
+npm link
+
+# npm install -g git://github.com/sumitparakh/microgateway.git#docker-dev-setup --registry=https://registry.npmjs.org
